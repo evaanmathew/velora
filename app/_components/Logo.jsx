20020b39da188d819cd245e0a26196e3585e8a6b
Evan Mathew <evanmathew2005@gmail.com>
2025-05-01 20:57:43 +0530

    feat: update routing to show landing page first and update logo link

--- app/_components/Logo.jsx
+++ app/_components/Logo.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function Logo() {
   return (
-    <Link href={'/dashboard'} className='flex items-center gap-2'>
+    <Link href={'/'} className='flex items-center gap-2'>
         <Image src={'/logo.png'} alt='logo'
         width={30} height={30}/>
         <h2 className='font-bold text-xl'>Velora</h2>
